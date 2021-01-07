93fc9d731a3e5e66bf7f497c7171c5530b0aff9f
Pjqa <>
2021-01-07 17:53:33 +0000

    added files

---
+++ src/com/qa/main/Runner.java
@@ -0,0 +1,29 @@
+package com.qa.main;
+
+public class Runner {
+
+	public static void main(String[] args) {
+	
+		//Calculator
+	
+	    int numb1 = 6;
+	    int numb2 = 24;
+	    
+	    int result1 = numb1 + numb2;
+	    int result2 = numb2 - numb1;
+	    int result3 = numb2 * numb1;
+	    
+	    double result4 = (double) numb1 / numb2;
+    
+	
+	    System.out.println(result1);
+	    System.out.println(result2);
+	    System.out.println(result3);
+	    System.out.println(result4);
+	    
+	
+}
+	    
+	    
+}
+	
